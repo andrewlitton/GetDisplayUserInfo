6d38151f670c4f12c5a92a22f7722f462bde6dbb
Drew <littonandrew@gmail.com>
2019-07-03 16:58:02 -0500

    completed getuserinfo project

---
+++ GetDisplayUserInfoProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GetDisplayUserInfoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInfo User = new UserInfo();
+            Console.WriteLine("Hello! What's your first name?");
+            User.firstName = Console.ReadLine();
+            Console.WriteLine();
+                        
+            Console.WriteLine("What's your last name?");
+            User.lastName = Console.ReadLine();
+            Console.WriteLine();
+            
+            Console.WriteLine("What's your favorite number?");
+            User.favoriteNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("How many pets do you have?");
+            User.numberOfPets = int.Parse(Console.ReadLine());
+            if (User.numberOfPets <= 0)
+            {
+                Console.WriteLine("Nice to meet you. Your name is " + User.firstName + " " + User.lastName + ". Your favorite number is " + User.favoriteNumber + ". You have no pets.");
+            }
+            else if (User.numberOfPets == 1)
+            {
+                Console.WriteLine("What is its name?");
+                User.namesOfPets = Console.ReadLine();
+                Console.WriteLine("Nice to meet you. Your name is " + User.firstName + " " + User.lastName + ". Your favorite number is " + User.favoriteNumber + ". You have one pet. Its name is " + User.namesOfPets + ".");
+            }
+            else
+            {
+                Console.WriteLine("What are their names?");
+                User.namesOfPets = Console.ReadLine();
+                Console.WriteLine("Nice to meet you. Your name is " + User.firstName + " " + User.lastName + ". Your favorite number is " + User.favoriteNumber + ". You have " + User.numberOfPets + " pets. Their names are " + User.namesOfPets + ".");
+            }
+            
+        }                
+    }
+}
---
+++ GetDisplayUserInfoProject/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetDisplayUserInfoProject
+{
+    class UserInfo
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int favoriteNumber { get; set; }
+        public int numberOfPets { get; set; }
+        public string namesOfPets { get; set; }
+        
+    }
+}
